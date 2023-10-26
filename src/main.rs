31a05ee482044088e6c022e0f7dc2c495681f563
Chad Scherrer <chad.scherrer@gmail.com>
2023-10-26 16:02:34 -0700

    separate mutable data from immutable

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,42 @@ use rayon::iter::{IndexedParallelIterator, ParallelIterator};
 use rv::prelude::*;
 use statrs::distribution::{ChiSquared, ContinuousCDF};
 
-const LOG_2PI: f64 =
-    1.837877066409345444578941147617480054291973978803466899914960778699346285617763;
+extern crate streaming_iterator as streamit;
+
+const LOG_2PI: f64 = 1.8378770664093456;
+
+fn is_symmetric(x: &DMatrix<f64>) -> bool {
+    let n = x.nrows();
+    assert_eq!(n, x.ncols());
+    for i in 0..n {
+        for j in 0..i {
+            if x[(i, j)] != x[(j, i)] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn symmetrize(x: &mut DMatrix<f64>) -> &DMatrix<f64> {
+    // check that x is square
+    let n = x.nrows();
+    assert_eq!(n, x.ncols());
+    for i in 0..n {
+        for j in 0..i {
+            x[(i, j)] += x[(j, i)];
+            x[(i, j)] /= 2.0;
+            x[(j, i)] = x[(i, j)];
+        }
+    }
+    debug_assert!(is_symmetric(x));
+    x
+}
 
 #[derive(Clone)]
 struct SuffStats {
-    n: usize,
-    k: usize,
+    num_samples: usize,
+    num_features: usize,
     xtx_eig: SymmetricEigen<f64, Dyn>, // Eigendecomposition of x' * x
     xty: DVector<f64>,                 // x' * y
     yty: f64,                          // y' * y
@@ -62,8 +91,8 @@ impl SuffStats {
         let yty = y.dot(y);
 
         SuffStats {
-            n,
-            k,
+            num_samples: n,
+            num_features: k,
             xtx_eig,
             xty,
             yty,
@@ -95,8 +124,8 @@ mod tests {
         let x = DMatrix::from_column_slice(3, 2, &[1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
         let y = DVector::from_column_slice(&[1.0, 2.0, 3.0]);
         let stats = SuffStats::new(&x, &y);
-        assert_eq!(stats.n, 3);
-        assert_eq!(stats.k, 2);
+        assert_eq!(stats.num_samples, 3);
+        assert_eq!(stats.num_features, 2);
         assert_eq!(stats.xty, DVector::from_column_slice(&[14.0, 32.0]));
         assert_eq!(stats.yty, 14.0);
     }
@@ -104,7 +133,6 @@ mod tests {
 
 #[derive(Clone)]
 struct Fit {
-    suffstats: SuffStats,
     weights: DVector<f64>,
     prior_precision: f64,
     noise_precision: f64,
@@ -114,94 +142,123 @@ struct Fit {
     update_noise: bool,
 }
 
-fn is_symmetric(x: &DMatrix<f64>) -> bool {
-    let n = x.nrows();
-    assert_eq!(n, x.ncols());
-    for i in 0..n {
-        for j in 0..i {
-            if x[(i, j)] != x[(j, i)] {
-                return false;
-            }
-        }
-    }
-    true
-}
-fn symmetrize(x: &mut DMatrix<f64>) -> &DMatrix<f64> {
-    // check that x is square
-    let n = x.nrows();
-    assert_eq!(n, x.ncols());
-    for i in 0..n {
-        for j in 0..i {
-            x[(i, j)] += x[(j, i)];
-            x[(i, j)] /= 2.0;
-            x[(j, i)] = x[(i, j)];
-        }
-    }
-    debug_assert!(is_symmetric(x));
-    x
-}
-
 impl Fit {
-    fn new(x: &DMatrix<f64>, y: &DVector<f64>) -> Fit {
-        let suffstats = SuffStats::new(x, y);
+    fn new(stats: &SuffStats) -> Fit {
         let prior_precision = 1.0;
         let noise_precision = 1.0;
 
         // Initialize the weights
         // This uses ridge regression in case xtx is singular
-        let eigvals: &DVector<f64> = &suffstats.xtx_eig.eigenvalues;
-        let eigvecs: &DMatrix<f64> = &suffstats.xtx_eig.eigenvectors;
+        let eigvals: &DVector<f64> = &stats.xtx_eig.eigenvalues;
+        let eigvecs: &DMatrix<f64> = &stats.xtx_eig.eigenvectors;
         let diag = DMatrix::from_diagonal(&eigvals.map(|lam| (1.0 + lam).inv()));
         let mut hinv = eigvecs * diag * eigvecs.transpose();
         symmetrize(&mut hinv);
-        let weights = hinv * &suffstats.xty;
+        let weights = hinv * &stats.xty;
 
-        let fit = Fit {
-            suffstats,
+        Fit {
             weights,
             prior_precision,
             noise_precision,
             iteration_count: 0,
             update_prior: true,
             update_noise: true,
-        };
-        fit
+        }
     }
 
-    fn ssr(&self) -> f64 {
-        self.suffstats.ssr(&self.weights)
+    // TODO: store inverse_hessian in the struct and update in-place
+    fn inverse_hessian(&self, stats: &SuffStats) -> DMatrix<f64> {
+        let eigvals: &DVector<f64> = &(stats.xtx_eig.eigenvalues);
+        let eigvecs: &DMatrix<f64> = &stats.xtx_eig.eigenvectors;
+        let diag = DMatrix::from_diagonal(
+            &eigvals.map(|lam| 1.0 / (self.prior_precision + self.noise_precision * lam)),
+        );
+        let mut hinv = eigvecs * diag * eigvecs.transpose();
+        symmetrize(&mut hinv);
+        debug_assert!(is_symmetric(&hinv));
+        hinv
     }
 
-    fn num_samples(&self) -> usize {
-        self.suffstats.n
+    fn update_weights(&mut self, stats: &SuffStats) {
+        let b = self.noise_precision;
+
+        let hinv = self.inverse_hessian(stats);
+        self.weights = hinv * &stats.xty;
+
+        self.weights *= b;
     }
 
-    fn num_features(&self) -> usize {
-        self.suffstats.k
-    }
-
-    fn xty(&self) -> &DVector<f64> {
-        &self.suffstats.xty
-    }
-
-    fn effective_num_parameters(&self) -> f64 {
+    fn effective_num_parameters(&self, stats: &SuffStats) -> f64 {
         let a = self.prior_precision;
         let b = self.noise_precision;
 
         // cache this to avoid recomputing
         let a_over_b = a / b;
 
-        let eigvals = &(self.suffstats.xtx_eig.eigenvalues);
+        let eigvals = &stats.xtx_eig.eigenvalues;
         let result = eigvals.iter().map(|lam| lam / (a_over_b + lam)).sum();
         debug_assert!(0.0 <= result);
-        debug_assert!(result <= self.num_features() as f64);
+        debug_assert!(result <= stats.num_samples as f64);
 
         result
     }
 
-    fn loglik_pvalue(&self, prior_precision: f64, noise_precision: f64) -> f64 {
+    fn ssr(&self, stats: &SuffStats) -> f64 {
+        let w = &self.weights;
+        stats.ssr(&w)
+    }
+
+    // TODO: Use this to add an Iterator trait implementation for Fit
+    fn update(&mut self, stats: &SuffStats) -> Option<&Self> {
+        let n = stats.num_samples as f64;
+
+        let w = &self.weights;
+
+        let wtw = w.dot(&w);
+        debug_assert!(wtw >= 0.0);
+
+        if self.update_prior {
+            self.prior_precision = self.effective_num_parameters(stats) / wtw;
+        }
+
+        if self.update_noise {
+            self.noise_precision = (n - self.effective_num_parameters(stats)) / self.ssr(stats);
+            debug_assert!(self.noise_precision > 0.0);
+        }
+        self.iteration_count += 1;
+
+        self.update_weights(stats);
+
+        Some(self)
+    }
+
+    fn logdet_hessian(&self, stats: &SuffStats) -> f64 {
+        let a = self.prior_precision;
+        let b = self.noise_precision;
+        debug_assert!(a >= 0.0);
+        debug_assert!(b >= 0.0);
+        let eigvals = &(stats.xtx_eig.eigenvalues);
+
+        eigvals.iter().map(|lam| (a + b * lam).ln()).sum()
+    }
+
+    fn log_evidence(&self, stats: &SuffStats) -> f64 {
+        let n = stats.num_samples as f64;
+        let k = stats.num_features as f64;
+
+        let a = self.prior_precision;
+        let b = self.noise_precision;
+        let rtr = self.ssr(stats);
+
+        0.5 * (k * a.ln() + n * b.ln()
+            - (b * rtr + a * self.weights.dot(&self.weights))
+            - self.logdet_hessian(stats)
+            - n * LOG_2PI)
+    }
+
+    fn loglik_pvalue(&self, stats: &SuffStats, prior_precision: f64, noise_precision: f64) -> f64 {
         // self serves as the alternative hypothesis
-        let loglik_alt = self.log_evidence();
+        let loglik_alt = self.log_evidence(stats);
 
         let mut freedom = 0.0;
         if self.update_prior {
@@ -212,85 +269,17 @@ impl Fit {
         }
 
         // The null hypothesis is the same, but with the prior and noise precisions set to the values passed in
-        let mut null = self.clone();
-        null.prior_precision = prior_precision;
-        null.noise_precision = noise_precision;
-        null.update_prior = false;
-        null.update_noise = false;
-        null.update();
-        let loglik_null = null.log_evidence();
+        let mut null_hyp = self.clone();
+        null_hyp.prior_precision = prior_precision;
+        null_hyp.noise_precision = noise_precision;
+        null_hyp.update_prior = false;
+        null_hyp.update_noise = false;
+
+        null_hyp.update(stats);
+        let loglik_null = null_hyp.log_evidence(stats);
 
         let test = ChiSquared::new(freedom).unwrap();
         test.cdf(2.0 * (loglik_alt - loglik_null))
-    }
-
-    fn log_evidence(&self) -> f64 {
-        let n = self.num_samples() as f64;
-        let k = self.num_features() as f64;
-
-        let a = self.prior_precision;
-        let b = self.noise_precision;
-        let rtr = self.ssr();
-
-        0.5 * (k * a.ln() + n * b.ln()
-            - (b * rtr + a * self.weights.dot(&self.weights))
-            - self.logdet_hessian()
-            - n * LOG_2PI)
-    }
-
-    fn logdet_hessian(&self) -> f64 {
-        let a = self.prior_precision;
-        let b = self.noise_precision;
-        debug_assert!(a >= 0.0);
-        debug_assert!(b >= 0.0);
-        let eigvals = &(self.suffstats.xtx_eig.eigenvalues);
-
-        eigvals.iter().map(|lam| (a + b * lam).ln()).sum()
-    }
-
-    // TODO: store inverse_hessian in the struct and update in-place
-    fn inverse_hessian(&self) -> DMatrix<f64> {
-        let eigvals: &DVector<f64> = &(self.suffstats.xtx_eig.eigenvalues);
-        let eigvecs: &DMatrix<f64> = &self.suffstats.xtx_eig.eigenvectors;
-        let diag = DMatrix::from_diagonal(
-            &eigvals.map(|lam| 1.0 / (self.prior_precision + self.noise_precision * lam)),
-        );
-        let mut hinv = eigvecs * diag * eigvecs.transpose();
-        symmetrize(&mut hinv);
-        debug_assert!(is_symmetric(&hinv));
-        hinv
-    }
-
-    fn update_weights(&mut self) {
-        let b = self.noise_precision;
-
-        let hinv = self.inverse_hessian();
-        self.weights = hinv * self.xty();
-
-        self.weights *= b;
-    }
-
-    // TODO: Use this to add an Iterator trait implementation for Fit
-    fn update(&mut self) {
-        let n = self.num_samples() as f64;
-
-        let w = &self.weights;
-
-        let wtw = w.dot(w);
-        debug_assert!(wtw >= 0.0);
-
-        if self.update_prior {
-            self.prior_precision = self.effective_num_parameters() / wtw;
-        }
-
-        if self.update_noise {
-            self.noise_precision = (n - self.effective_num_parameters()) / self.ssr();
-            debug_assert!(self.noise_precision > 0.0);
-        }
-
-        self.update_weights();
-
-        self.iteration_count += 1;
     }
 }
 
@@ -324,13 +313,14 @@ fn main() {
         let prior_precision = 7.0;
         let noise_precision = 11.0;
         let (_w, y) = fake_data(&x, prior_precision, noise_precision);
-        let mut fit = Fit::new(&x, &y);
+        let stats = SuffStats::new(&x, &y);
+        let mut fit = Fit::new(&stats);
 
         // 10 iterations. This is very kludgy; we should use an Iterator trait implementation instead.
         for _n in 0..10 {
-            fit.update();
+            fit.update(&stats);
         }
-        let pval = fit.loglik_pvalue(prior_precision, noise_precision);
+        let pval = fit.loglik_pvalue(&stats, prior_precision, noise_precision);
         pval_sum += pval;
     }
 
